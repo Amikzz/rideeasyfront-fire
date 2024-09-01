5689756117e6b7b9c78a6b8954480ab7a5face64
Amika Subasinghe <amixx2005@gmail.com>
2024-09-02 01:43:20 +0530

    1.43 pm commit

--- lib/features/HomePage/home.dart
+++ lib/features/HomePage/home.dart
@@ -12,6 +12,8 @@ import 'package:ride_easy/features/TicketBooking/ticketbooking.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert';
+import 'package:kommunicate_flutter/kommunicate_flutter.dart';
+import 'package:ride_easy/features/WelcomePage/welcome.dart';
 
 
 class HomePage extends StatefulWidget {
@@ -88,7 +90,7 @@ class _HomePageState extends State<HomePage> {
       await FirebaseAuth.instance.signOut();
       Navigator.pushReplacement(
         context,
-        MaterialPageRoute(builder: (context) => const Placeholder()), // Replace with your login page
+        MaterialPageRoute(builder: (context) => const WelcomePage()), // Replace with your login page
       );
     } catch (e) {
       print('Error signing out: ${e.toString()}');
@@ -245,6 +247,69 @@ class _HomePageState extends State<HomePage> {
     }
   }
 
+  Future<void> _launchChatbot() async {
+    try {
+      // Prepare the conversation object with your Kommunicate app ID
+      dynamic conversationObject = {
+        'appId': '243c79d5281787463f0c33ba1582fef6', // Replace with your actual Kommunicate app ID
+      };
+
+      // Show chatbot in a dialog
+      showDialog(
+        context: context,
+        builder: (BuildContext context) {
+          return Dialog(
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(20.0),
+            ),
+            child: Container(
+              width: double.maxFinite,
+              padding: const EdgeInsets.all(20),
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                children: <Widget>[
+                  const Text(
+                    'Chat with Us',
+                    style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+                  ),
+                  const SizedBox(height: 10),
+                  FutureBuilder(
+                    future: KommunicateFlutterPlugin.buildConversation(conversationObject),
+                    builder: (context, snapshot) {
+                      if (snapshot.connectionState == ConnectionState.waiting) {
+                        return const CircularProgressIndicator();
+                      } else if (snapshot.hasError) {
+                        return const Text('Error launching chatbot');
+                      } else {
+                        return const Text('Chatbot is ready');
+                      }
+                    },
+                  ),
+                  const SizedBox(height: 20),
+                  ElevatedButton(
+                    onPressed: () {
+                      Navigator.of(context).pop(); // Close the dialog
+                    },
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: Colors.green,
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                    ),
+                    child: const Text('Close Chatbot'),
+                  ),
+                ],
+              ),
+            ),
+          );
+        },
+      );
+    } catch (e) {
+      print("Error launching chatbot: " + e.toString());
+    }
+  }
+
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -257,7 +322,7 @@ class _HomePageState extends State<HomePage> {
             mainAxisAlignment: MainAxisAlignment.spaceBetween,
             children: [
               Padding(
-                padding: EdgeInsets.all(8.0),
+                padding: const EdgeInsets.all(8.0),
                 child: Row(
                   mainAxisAlignment: MainAxisAlignment.spaceBetween,
                   children: [
@@ -268,7 +333,7 @@ class _HomePageState extends State<HomePage> {
                         fontWeight: FontWeight.bold,
                       ),
                     ),
-                    SizedBox(width: 70) // Adjust space to accommodate the dropdown
+                    const SizedBox(width: 70) // Adjust space to accommodate the dropdown
                   ],
                 ),
               ),
@@ -284,7 +349,7 @@ class _HomePageState extends State<HomePage> {
                   } else if (value == 'profile') {
                     Navigator.pushReplacement(
                       context,
-                      MaterialPageRoute(builder: (context) => const ProfilePage()), // Replace with your login page
+                      MaterialPageRoute(builder: (context) => const ProfilePage()),
                     );
                   }
                 },
@@ -300,122 +365,135 @@ class _HomePageState extends State<HomePage> {
                     ),
                   ),
                   const PopupMenuItem<String>(
-                      value: 'profile',
-                      child: Row(
-                        children: [
-                          Icon(Icons.settings, color: Colors.black),
-                          SizedBox(width: 8),
-                          Text('Profile'),
-                        ],
-                      )
-                  )
+                    value: 'profile',
+                    child: Row(
+                      children: [
+                        Icon(Icons.settings, color: Colors.black),
+                        SizedBox(width: 8),
+                        Text('Profile'),
+                      ],
+                    ),
+                  ),
                 ],
               ),
             ],
           ),
         ),
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(20.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: [
-            const SizedBox(height: 10),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      body: Stack(
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(20.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
               children: [
-                const Padding(
-                  padding: EdgeInsets.only(left: 10),
-                  child: Text(
-                    'Now',
-                    style: TextStyle(
-                      fontSize: 16,
-                      fontWeight: FontWeight.bold,
+                const SizedBox(height: 10),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    const Padding(
+                      padding: EdgeInsets.only(left: 10),
+                      child: Text(
+                        'Now',
+                        style: TextStyle(
+                          fontSize: 16,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ),
+                    Text(
+                      _cityName,
+                      style: const TextStyle(
+                        fontSize: 16,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.green,
+                      ),
+                    ),
+                    const Icon(Icons.location_on, color: Colors.green),
+                  ],
+                ),
+                const SizedBox(height: 32),
+                Expanded(
+                  child: GridView.count(
+                    crossAxisCount: 2,
+                    crossAxisSpacing: 16,
+                    mainAxisSpacing: 16,
+                    children: [
+                      _buildButton(
+                        context: context,
+                        title: 'Bus Tracking',
+                        targetPage: const BusTrackingPage(),
+                        image: const AssetImage('assets/images/track.png'),
+                      ),
+                      _buildButton(
+                        context: context,
+                        title: 'Bus Schedule',
+                        image: const AssetImage('assets/images/shedule.png'),
+                        targetPage: const BusSchedulePage(),
+                      ),
+                      _buildButton(
+                        context: context,
+                        title: 'Ticket Booking',
+                        image: const AssetImage('assets/images/ticket.png'),
+                        targetPage: const TicketBookingPage(),
+                      ),
+                      _buildButton(
+                        context: context,
+                        title: 'Bus Seat Reserve',
+                        image: const AssetImage('assets/images/seat.png'),
+                        targetPage: const DestinationPage(),
+                      ),
+                      _buildButton(
+                        context: context,
+                        title: 'Support',
+                        image: const AssetImage('assets/images/support.png'),
+                        targetPage: const FAQPage(),
+                      ),
+                      _buildButton(
+                        context: context,
+                        title: 'FeedBack',
+                        image: const AssetImage('assets/images/feedback.png'),
+                        targetPage: const FeedbackPage(),
+                      ),
+                    ],
+                  ),
+                ),
+                const SizedBox(height: 16),
+                SizedBox(
+                  width: 80,
+                  height: 80,
+                  child: ElevatedButton(
+                    onPressed: () {
+                      _showSafetyDialog();
+                    },
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: Colors.red,
+                      shape: const CircleBorder(),
+                    ),
+                    child: const Text(
+                      'Safety',
+                      style: TextStyle(
+                        fontSize: 18,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.white,
+                      ),
+                      textAlign: TextAlign.center,
                     ),
                   ),
                 ),
-                Text(
-                  _cityName,
-                  style: const TextStyle(
-                    fontSize: 16,
-                    fontWeight: FontWeight.bold,
-                    color: Colors.green,
-                  ),
-                ),
-                const Icon(Icons.location_on, color: Colors.green),
               ],
             ),
-            const SizedBox(height: 32),
-            Expanded(
-              child: GridView.count(
-                crossAxisCount: 2,
-                crossAxisSpacing: 16,
-                mainAxisSpacing: 16,
-                children: [
-                  _buildButton(
-                    context: context,
-                    title: 'Bus Tracking',
-                    targetPage: const BusTrackingPage(),
-                    image: const AssetImage('assets/images/track.png'),
-                  ),
-                  _buildButton(
-                    context: context,
-                    title: 'Bus Schedule',
-                    image: const AssetImage('assets/images/shedule.png'),
-                    targetPage: const BusSchedulePage(),
-                  ),
-                  _buildButton(
-                    context: context,
-                    title: 'Ticket Booking',
-                    image: const AssetImage('assets/images/ticket.png'),
-                    targetPage: const TicketBookingPage(),
-                  ),
-                  _buildButton(
-                    context: context,
-                    title: 'Bus Seat Reserve',
-                    image: const AssetImage('assets/images/seat.png'),
-                    targetPage: const DestinationPage(),
-                  ),
-                  _buildButton(
-                    context: context,
-                    title: 'Support',
-                    image: const AssetImage('assets/images/support.png'),
-                    targetPage: const FAQPage(),
-                  ),
-                  _buildButton(
-                    context: context,
-                    title: 'FeedBack',
-                    image: const AssetImage('assets/images/feedback.png'),
-                    targetPage: const FeedbackPage(),
-                  ),
-                ],
-              ),
+          ),
+          Positioned(
+            bottom: 35,
+            right: 30,
+            child: FloatingActionButton(
+              onPressed: _launchChatbot,
+              backgroundColor: Colors.green,
+              child: const Icon(Icons.chat, color: Colors.white,),
             ),
-            const SizedBox(height: 16),
-            SizedBox(
-              width: 80,
-              height: 80,
-              child: ElevatedButton(
-                onPressed: () {
-                  _showSafetyDialog();
-                },
-                style: ElevatedButton.styleFrom(
-                  backgroundColor: Colors.red,
-                  shape: const CircleBorder(),
-                ),
-                child: const Text(
-                  'Safety',
-                  style: TextStyle(
-                    fontSize: 18,
-                    fontWeight: FontWeight.bold,
-                    color: Colors.white,
-                  ),
-                  textAlign: TextAlign.center,
-                ),
-              ),
-            ),
-          ],
-        ),
+          ),
+        ],
       ),
     );
   }
